c5322554f4cdbdf2e625a00ff34a5b08e15a0351
Victor Lustoza <144399806+victorlustoza@users.noreply.github.com>
2024-01-29 18:05:27 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,6 @@ module.exports = defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
   defaultCommandTimeout: 15000,
-  retries: 2,
   reporter: 'cypress-multi-reporters',
   reporterOptions: {
     reporterEnabled: 'cypress-mochawesome-reporter, cypress-qase-reporter',
